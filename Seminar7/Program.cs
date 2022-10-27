f6e340267b7b48bdfebbb20fc9660746d9321f14
olegPip <olegpipuli@mail.ru>
2022-10-27 12:52:27 +0300

    example7

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -1,7 +1,48 @@
-﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+﻿
+// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Enter the number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+double[,] numbers = new double[rows, columns];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+      
+        Console.WriteLine("");
+    }
+}
 
 
 // Задача 2. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+
+
+
+
+
 
 
 // Задача 3.  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
