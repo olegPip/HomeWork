075d3285416b5ccf9d9c3e576324a72532016c92
olegPip <olegpipuli@mail.ru>
2022-11-12 21:20:55 +0300

    seminar 8 task2

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -43,7 +43,60 @@ Console.WriteLine();
 Show2dArray(myArray);
 */
 //Задача 2. Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+/*
 
+int[,] CreateRandom2dArray(int rows, int columns, int minVal, int maxVal)
+{
+  int[,] array = new int[rows, columns];
+  for (int i = 0; i < rows; i++)
+    for (int j = 0; j < columns; j++)
+      array[i, j] = new Random().Next(minVal, maxVal + 1);
+  return array;
+}
+
+void Show2dArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+      Console.Write(array[i, j] + " ");
+    Console.WriteLine();
+  }
+}
+
+void MaxSumRow2dArray(int[,] array)
+{
+  if (array.GetLength(0) != array.GetLength(1))
+  {
+    int[] sumRowArray = new int[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+      int num = 0;
+      for (int j = 0; j < array.GetLength(1); j++)
+        num += array[i, j];
+      sumRowArray[i] = num;
+    }
+    int sumMinRow = sumRowArray[0];
+    int indexSumMinRow = 0;
+
+    for (int i = 1; i < sumRowArray.Length; i++)
+      if (sumMinRow > sumRowArray[i])
+      {
+        sumMinRow = sumRowArray[i];
+        indexSumMinRow = i;
+      }
+    Console.WriteLine($"The row with the smallest sum of elements - {indexSumMinRow + 1} line amount - {sumMinRow}");
+  }
+  else
+    Console.WriteLine("Square array");
+}
+
+int[,] myArray = CreateRandom2dArray(5, 6, 1, 5);
+Show2dArray(myArray);
+Console.WriteLine();
+MaxSumRow2dArray(myArray);
+
+*/
 
 //Задача 3. Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 
